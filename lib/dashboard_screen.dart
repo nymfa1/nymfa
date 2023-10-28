8b32b19bb4e0fe97ec80226d957001893ecf26d0
nymfa1 <149262785+nymfa1@users.noreply.github.com>
2023-10-29 00:36:37 +0700

    Add files via upload

---
+++ lib/dashboard_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                "assets/BPolos.jpg"), // Ganti dengan path ke gambar latar belakang
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Selamat datang di Dashboard",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              DashboardIconsGrid(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profil"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 160,
+              height: 160,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 4),
+              ),
+              child: const CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage(
+                    'assets/avm.jpg'), // Ganti dengan path ke gambar profil
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Nama: Arivian Mujahid",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "NPM: 21670007",
+              style: TextStyle(fontSize: 18),
+            ),
+            const Text(
+              "Kelas: 5A",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardIconsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      children: <Widget>[
+        DashboardIcon(
+          icon: Icons.person,
+          label: "Profil", // Tambahkan label sesuai ikon
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProfileScreen(),
+              ),
+            );
+          },
+        ),
+        DashboardIcon(
+          icon: Icons.gamepad,
+          label: "Favorit", // Tambahkan label sesuai ikon
+        ),
+        DashboardIcon(
+          icon: Icons.email,
+          label: "Email", // Tambahkan label sesuai ikon
+        ),
+        
+        DashboardIcon(
+          icon: Icons.home_filled,
+          label: "Alamat", // Tambahkan label sesuai ikon
+        ),
+        
+      ],
+    );
+  }
+}
+
+class DashboardIcon extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+  final Color iconColor; // Tambahkan properti warna ikon
+
+  DashboardIcon({
+    required this.icon,
+    required this.label,
+    this.onPressed = _dummyOnPressed,
+    this.iconColor = Colors.blue, // Warna ikon default adalah hitam
+  });
+
+  static void _dummyOnPressed() {
+    print("Tombol belum dikonfigurasi.");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              icon,
+              color: iconColor, // Gunakan warna ikon yang telah ditentukan
+            ),
+            iconSize: 48,
+            onPressed: onPressed,
+          ),
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
